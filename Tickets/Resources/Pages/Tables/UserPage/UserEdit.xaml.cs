67d8a4b18d132f714b2cc7fea8d40d95d2b868d7
MiaoRiDzo <ya.eltyshev@gmail.com>
2024-05-30 11:41:31 +0500

    0.0.2 Edits and Search --Users table-- [+] Add Search users by name [+]Add Edit Page

--- Tickets/Resources/Pages/Tables/UserPage/UserEdit.xaml.cs
+++ Tickets/Resources/Pages/Tables/UserPage/UserEdit.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,8 +14,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 using Tickets.Resources.LibsAndDictionary;
 using Tickets.Resources.LibsAndDitcionary;
+using TicketSeller.Resources.LibsAndDictionary;
 
 namespace Tickets.Resources.Pages.Tables.UserPage
 {
@@ -22,12 +26,68 @@ namespace Tickets.Resources.Pages.Tables.UserPage
     /// </summary>
     public partial class UserEdit : Page
     {
-        User _current;
+        User _current = new User();
+        bool newIns = true;
         public UserEdit(User user)
         {
             InitializeComponent();
-            _current = user;
             AppData.dockPanel.Children.Clear();
+
+            //init page
+            if (user != null)
+            {
+                _current = user;
+                newIns = false;
+            }
+            cbRole.ItemsSource = AppData.getContext().Role.ToList();
+            DataContext = _current;
+            
+        }
+        private static int getNextId(User current)
+        {
+            try
+            {
+                int id = AppData.getContext().User.ToList().Last().UserID;
+                return id + 1;
+            }
+            catch { return 0; }
+        }
+
+        private void btn_save_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (String.IsNullOrEmpty(tb_fullName.Text)) errors.AppendLine("Введите имя польхователя;");
+            if (String.IsNullOrEmpty(tb_login.Text)) errors.AppendLine("Введите логин;");
+            if (String.IsNullOrEmpty(tb_password.Password)) errors.AppendLine("Введите пароль;");
+            if (cbRole.SelectedItem == null) errors.AppendLine("Выберите должность;");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+
+            if (_current.UserID >= 0)
+            {
+                _current.UserPassword = MD5Hash.StringToMD5(tb_password.Password);
+                if (newIns)
+                {
+                    _current.UserID = getNextId(_current);
+                    Role role = cbRole.SelectedItem as Role;
+                    _current.RoleID = role.RoleID;
+                }
+                AppData.getContext().User.AddOrUpdate(_current);
+                try
+                {
+                    AppData.getContext().SaveChanges();
+                    MessageBox.Show("Данные сохранены.");
+                    AppData.mFrame.GoBack();
+                }catch(Exception ex)
+                {
+                    MessageBox.Show("Ошибка\n" + ex.Message);
+                }
+            }
         }
     }
 }
--- Tickets/Resources/Pages/Tables/UserPage/ViewUser.xaml.cs
+++ Tickets/Resources/Pages/Tables/UserPage/ViewUser.xaml.cs
@@ -27,7 +27,43 @@ namespace Tickets.Resources.Pages.Tables.UserPage
             InitializeComponent();
             dg_user.ItemsSource = AppData.getContext().User.ToList();
             
-            AppData.AddDock<User>(dg_user, () => new UserPage.UserEdit(null));
+            
+        }
+
+        private void editBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AppData.mFrame.Navigate(new UserPage.UserEdit((sender as Button).DataContext as User));
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            AppData.AddDock<User>(dg_user, () => AppData.mFrame.Navigate(new UserPage.UserEdit(null)));
+            AppData.getContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+            dg_user.ItemsSource = AppData.getContext().User.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            dg_user.ItemsSource = AppData.getContext().User.ToList();
+
+            if (!string.IsNullOrEmpty(tb_search.Text))
+            {
+                // Предположим, что у вас есть DataGridView с именем dgUsers
+                List<User> list = dg_user.ItemsSource as List<User>; // Получаем List<User> из DataSource
+                if (list != null) // Проверяем, что он не null
+                {
+                    string filter = tb_search.Text; // Получаем текст из TextBox
+                    List<User> filteredList = list.FindAll(user =>
+                        user.UserName.IndexOf(filter, StringComparison.OrdinalIgnoreCase) >= 0); // Создаем отфильтрованный список по лямбда-выражению
+                    dg_user.ItemsSource = filteredList; // Привязываем отфильтрованный список к DataGridView
+                }
+            }
+            else
+            {
+                dg_user.ItemsSource = AppData.getContext().User.ToList();
+            }
+
         }
     }
+    
 }
